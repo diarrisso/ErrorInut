0a3006a12262d6196989a89d282ba52bfb4b25a3
mamadidiarrisso <diarrisso49@gmail.com>
2023-01-22 00:07:46 +0100

    insert in datatbase msql

--- ajax.php
+++ ajax.php
@@ -53,8 +53,43 @@ if( $_FILES['upload']['error'] == 0 ) {
 
             $nummer  = explode(':', $row[1]);
 
+        $c = '5';
 
-        if ($nummer[0] == "2100000002002300" ) {
+            switch ($nummer[0]) {
+                case "2100000002002300":
+                    $insertQuery = "INSERT INTO Blog.tbl_users( 
+                           tbl_users.userName,
+                           tbl_users.firstName, 
+                           tbl_users.bwertung,
+                           tbl_users.interviewID,
+                           tbl_users.bussines,
+                            tbl_users.nps,
+                           tbl_users.queu,
+                           tbl_users.bemerkung
+                           )VALUES ('" . $row[5] . "', '" . $row[6] . "', '" . $c[0] . "', '" . $row[0] . "', '" . $row[8] . "', '" . $row[4] . "','" . $row[3] . "',  '" . $row[9] . "')";
+                    $statement = $connection->prepare($insertQuery);
+                    $statement->execute();
+                    break;
+                case "21900000002000500":
+                    $insertQuery = "INSERT INTO Blog.tbl_users( 
+                           tbl_users.userName,
+                           tbl_users.firstName, 
+                           tbl_users.bwertung,
+                           tbl_users.interviewID,
+                           tbl_users.bussines,
+                            tbl_users.nps,
+                           tbl_users.queu,
+                           tbl_users.bemerkung
+                           )VALUES ('" . $row[5] . "', '" . $row[6] . "', '" . $c[0] . "', '" . $row[0] . "', '" . $row[8] . "', '" . $row[4] . "','" . $row[3] . "',  '" . $row[9] . "')";
+                    $statement = $connection->prepare($insertQuery);
+                    $statement->execute();
+                    break;
+                default :
+                    'salut ';
+            }
+
+
+       if ($nummer[0] == "2100000002002300" ) {
 
 
             $c =  "5";
@@ -74,7 +109,7 @@ if( $_FILES['upload']['error'] == 0 ) {
         }
 
 
-        if ($nummer[0] == "2100000002000400") {
+        if ($nummer[0] == "21900000002000500") {
 
 
             $c = '5';
@@ -115,14 +150,25 @@ if( $_FILES['upload']['error'] == 0 ) {
         }
 
 
+        if ($nummer[0] == '21900000002000500') {
 
 
-
-
-
-
-
-
+            $c = '5';
+            echo $c;
+            $insertQuery = "INSERT INTO Blog.tbl_users( 
+                           tbl_users.userName,
+                           tbl_users.firstName, 
+                           tbl_users.bwertung,
+                           tbl_users.interviewID,
+                           tbl_users.bussines,
+                            tbl_users.nps,
+                           tbl_users.queu,
+                           tbl_users.bemerkung
+                           )VALUES ('" . $row[5] . "', '" . $row[6] . "', '" . $c . "', '" . $row[0] . "', '" . $row[8] . "', '" . $row[4] . "','" . $row[3] . "',  '" . $row[9] . "')";
+            $statement = $connection->prepare($insertQuery);
+            $statement->execute();
+             exit();
+        }
 
 
         if ($row[0]) {
