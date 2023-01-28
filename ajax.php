2a78cb1fa10206410eaff79d1ca266a1b89398a5
mamadidiarrisso <diarrisso49@gmail.com>
2023-01-28 15:07:28 +0100

    update tkschat ignore

--- ajax.php
+++ ajax.php
@@ -24,193 +24,141 @@ if( $_FILES['upload']['error'] == 0 ) {
 
     $csv_data = array_map('str_getcsv', file($tmpName));
 
-    /*array_walk($csv_data , function(&$x) use ($csv_data) {
-        $x = array_combine($csv_data[0], $x);
-    });*/
-
     /**
      *
      * array_shift = remove first value of array
      * in csv file header was the first value
      *
      */
-    array_shift($csv_data);
-
-
-
-// Print Result Data
-
-
-
+    //array_shift($csv_data);
 
     $CSVData = array();
     $output = array();
     foreach ( $csv_data as $key => $row ) {
-        unset($row[12]);
-        unset($row[11]);
-        unset($row[14]);
-        unset($row[13]);
+        $separator = ':';
+        $fragen = explode( $separator, $row[1]) ;
+        $nom = explode(" ", $row[3]);
+        $prenom = $nom[0];
+        $nachnme = $nom[1];
+        $fragen = trim( $fragen[0]);
+        $column = $row[0];
+        unset($row[0]);
+        $csvDatum = "2023-01-11";
 
-            $nummer  = explode(':', $row[1]);
-
-        $c = '5';
-
-            switch ($nummer[0]) {
-                case "2100000002002300":
-                    $insertQuery = "INSERT INTO Blog.tbl_users( 
-                           tbl_users.userName,
-                           tbl_users.firstName, 
-                           tbl_users.bwertung,
-                           tbl_users.interviewID,
-                           tbl_users.bussines,
-                            tbl_users.nps,
-                           tbl_users.queu,
-                           tbl_users.bemerkung
-                           )VALUES ('" . $row[5] . "', '" . $row[6] . "', '" . $c[0] . "', '" . $row[0] . "', '" . $row[8] . "', '" . $row[4] . "','" . $row[3] . "',  '" . $row[9] . "')";
-                    $statement = $connection->prepare($insertQuery);
-                    $statement->execute();
-                    break;
-                case "21900000002000500":
-                    $insertQuery = "INSERT INTO Blog.tbl_users( 
-                           tbl_users.userName,
-                           tbl_users.firstName, 
-                           tbl_users.bwertung,
-                           tbl_users.interviewID,
-                           tbl_users.bussines,
-                            tbl_users.nps,
-                           tbl_users.queu,
-                           tbl_users.bemerkung
-                           )VALUES ('" . $row[5] . "', '" . $row[6] . "', '" . $c[0] . "', '" . $row[0] . "', '" . $row[8] . "', '" . $row[4] . "','" . $row[3] . "',  '" . $row[9] . "')";
-                    $statement = $connection->prepare($insertQuery);
-                    $statement->execute();
-                    break;
-                default :
-                    'salut ';
-            }
-
-
-       if ($nummer[0] == "2100000002002300" ) {
-
-
-            $c =  "5";
-            $insertQuery = "INSERT INTO Blog.tbl_users( 
-                           tbl_users.userName,
-                           tbl_users.firstName, 
-                           tbl_users.bwertung,
-                           tbl_users.interviewID,
-                           tbl_users.bussines,
-                            tbl_users.nps,
-                           tbl_users.queu,
-                           tbl_users.bemerkung
-                           )VALUES ('".$row[5]."', '".$row[6]."', '".$c[0]."', '".$row[0]."', '".$row[8]."', '".$row[4]."','".$row[3]."',  '".$row[9]."')";
-            $statement = $connection->prepare($insertQuery);
-            $statement->execute();
-
+        if ($row[7] != $csvDatum ) {
+            continue;
         }
 
+        error_log('datum =>'.print_r( $row[7] ) );
+        /*$date = date_create($row[7]);
+        $newDatas = date_format($date, 'Y-m-d');*/
+        $output['benitzer'] = $row[6];
+        $output['nom'] = $row[5];
+        $output['studie'] = $row[8];
+        $output['email'] = $row[3];
+        $output['email'] = $row[4];
+        $output['queue'] = $row[9];
+        $output['date'] = $row[7];
+        $output[$column]['fragenAntworte'][$fragen] = $row[2];
+        $CSVData[$column] = $output;
 
-        if ($nummer[0] == "21900000002000500") {
+    }
+    error_log(' array => '.print_r($CSVData, true));
 
-
-            $c = '5';
-            echo $c;
-            $insertQuery = "INSERT INTO Blog.tbl_users( 
-                           tbl_users.userName,
-                           tbl_users.firstName, 
-                           tbl_users.bwertung,
-                           tbl_users.interviewID,
-                           tbl_users.bussines,
-                            tbl_users.nps,
-                           tbl_users.queu,
-                           tbl_users.bemerkung
-                           )VALUES ('" . $row[5] . "', '" . $row[6] . "', '" . $c. "', '" . $row[0] . "', '" . $row[8] . "', '" . $row[4] . "','" . $row[3] . "',  '" . $row[9] . "')";
-            $statement = $connection->prepare($insertQuery);
-            $statement->execute();
-
-        }
-
-
-        if ($nummer[0] == "2100000002001300") {
-
-
-            $c = '5';
-            $insertQuery = "INSERT INTO Blog.tbl_users( 
-                           tbl_users.userName,
-                           tbl_users.firstName, 
-                           tbl_users.bwertung,
-                           tbl_users.interviewID,
-                           tbl_users.bussines,
-                            tbl_users.nps,
-                           tbl_users.queu,
-                           tbl_users.bemerkung
-                           )VALUES ('" . $row[5] . "', '" . $row[6] . "', '" . $c. "', '" . $row[0] . "', '" . $row[8] . "', '" . $row[4] . "','" . $row[3] . "',  '" . $row[9] . "')";
-            $statement = $connection->prepare($insertQuery);
-            $statement->execute();
-
-        }
-
-
-        if ($nummer[0] == '21900000002000500') {
-
-
-            $c = '5';
-            echo $c;
-            $insertQuery = "INSERT INTO Blog.tbl_users( 
-                           tbl_users.userName,
-                           tbl_users.firstName, 
-                           tbl_users.bwertung,
-                           tbl_users.interviewID,
-                           tbl_users.bussines,
-                            tbl_users.nps,
-                           tbl_users.queu,
-                           tbl_users.bemerkung
-                           )VALUES ('" . $row[5] . "', '" . $row[6] . "', '" . $c . "', '" . $row[0] . "', '" . $row[8] . "', '" . $row[4] . "','" . $row[3] . "',  '" . $row[9] . "')";
-            $statement = $connection->prepare($insertQuery);
-            $statement->execute();
-             exit();
-        }
-
-
-        if ($row[0]) {
-            $interwiewIde = $row[0];
-        }
-
-        if ($row[5]) {
-            $vormane = $row[5];
-        }
-
-        if ($row[6]) {
-            $nachname = $row[6];
-        }
+    /*if ( strpos($row[8], 'Wholebuy') ) {
+        error_log('business = > business WB');
+        echo 'business WB';
+    } else {
+        error_log('business ');
+        echo 'business ';
+    }
 
 
 
+    if (! strpos($row[8], 'Wholebuy') ) {
+        error_log('business TKS');
+        echo 'business WB';
+    }
 
-        /*switch ($nummer) {
 
-            case "2100000002000400":
+    if ( strpos($row[1], "210", 1) ) {
+        error_log('offset');
+        echo 'offset';
+    }
 
 
-                break;
-            case '2100000002001800':
-                echo 'ja';
-                break;
-            default :
-                'cool';
-        }*/
-        $CSVData = $row;
+    $start = $row[1];
+
+    if ( $start[0] === "2" && $start[1] === "1" && $start[2] === "0" ){
+        error_log("coole");
 
     }
 
-    error_log('row => '  . print_r( $CSVData, true ) );
+    if ($start[0] === '4' && $start[1] === '1' && $start[2] === '0') {
+        error_log('coole');
+        exit();
+    }
 
 
-    json_encode($csv_data);
+if (strlen($row[1]) === "210") {
+error_log("salut ");
+}
+
+
+    $nummer  = explode(':', $row[1]);
+
+          $c = '5';
+
+    if ($row[0]) {
+        $interwiewIde = $row[0];
+    }
+
+    if ($row[5]) {
+        $vormane = $row[5];
+    }
+
+    if ($row[6]) {
+        $nachname = $row[6];
+    }*/
+
+
+
+
+    /*switch ($nummer) {
+
+        case "2100000002000400":
+
+
+            break;
+        case '2100000002001800':
+            echo 'ja';
+            break;
+        default :
+            'cool';
+    }*/
+
+
+    //error_log('le premier foreach => '  .print_r( $CSVData, true ) );
+
+
+
+
+
+    /*  $data = array();
+    foreach ( $CSVData  as  $interviewId => $value ) {
+
+        $data['interviewId'] = $interviewId ;
+        $data = $value;
+
+
+    }*/
+
+
 
 }
 
 
-function array_group(array $data, $by_column)
+function array_group( array $data, $by_column )
 {
     $result = [];
     foreach ($data as $item) {
