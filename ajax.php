f3640ed0f0f3ca380cdd9b07ba0fe518fb1a7807
mamadidiarrisso <diarrisso49@gmail.com>
2023-01-29 19:11:12 +0100

    update tkschat ignore sum array, regrouper les chats by Datum et par date_format,array splite

--- ajax.php
+++ ajax.php
@@ -30,6 +30,41 @@ if( $_FILES['upload']['error'] == 0 ) {
      * in csv file header was the first value
      *
      */
+
+
+    $result = null;
+    $teste =  array( array( '12345566666',
+                     'diarrisso',
+                     'saran',
+                     4,
+                     4,
+                     4,
+                     4
+                 ),
+             );
+    $newArray = array();
+    $data = array();
+    foreach ( $teste as $key => $item ) {
+
+        error_log('items dans foreach  => ' . print_r($item, true));
+
+        $data['id'] = $item[0];
+        $data['nom'] = $item[1];
+        $data['prenom'] = $item[2];
+        $data['int1'] = $item[3];
+        $data['int2'] = $item[4];
+        $data['int3'] = $item[5];
+        $data['int4'] = $item[6];
+        $newArray = $data;
+        $output = array_slice( $newArray, 1);
+        $result = array_sum($output);
+}
+        $newArray['sum'] = $result;
+    error_log('$rsulte de =>'.print_r( $result, true ) );
+    error_log('$data =>'.print_r( $newArray, true ) );
+    error_log('$output =>'.print_r( $output, true ) );
+
+    die();
     //array_shift($csv_data);
 
     $CSVData = array();
@@ -37,29 +72,44 @@ if( $_FILES['upload']['error'] == 0 ) {
     foreach ( $csv_data as $key => $row ) {
         $separator = ':';
         $fragen = explode( $separator, $row[1]) ;
-        $nom = explode(" ", $row[3]);
+        /*$nom = explode(" ", $row[4]);
         $prenom = $nom[0];
-        $nachnme = $nom[1];
+        $nachnme = $nom[1];*/
         $fragen = trim( $fragen[0]);
         $column = $row[0];
         unset($row[0]);
         $csvDatum = "2023-01-11";
 
-        if ($row[7] != $csvDatum ) {
+        /*if ($row[7] != $csvDatum ) {
             continue;
+        }*/
+
+        if (!in_array($csvDatum, $row) ) {
+            continue;
+            //error_log('oui il \'est pas dedant ');
+
         }
 
+        if ($key === 1) {
+
+            continue;
+
+        }
+
+        $result = array_sum($row);
+
         error_log('datum =>'.print_r( $row[7] ) );
-        /*$date = date_create($row[7]);
-        $newDatas = date_format($date, 'Y-m-d');*/
-        $output['benitzer'] = $row[6];
-        $output['nom'] = $row[5];
-        $output['studie'] = $row[8];
-        $output['email'] = $row[3];
-        $output['email'] = $row[4];
-        $output['queue'] = $row[9];
-        $output['date'] = $row[7];
-        $output[$column]['fragenAntworte'][$fragen] = $row[2];
+        error_log('resulte =>'.$result);
+        $date = date_create($row[7]);
+        $newDatas = date_format($date, 'Y-m-d');
+        $output[$row[7]]['benitzer'] = $row[6];
+        $output[$row[7]]['nom'] = $row[5];
+        $output[$row[7]]['studie'] = $row[8];
+        $output[$row[7]]['email'] = $row[3];
+        $output[$row[7]]['email'] = $row[4];
+        $output[$row[7]]['queue'] = $row[9];
+        $output[$row[7]]['date'] = $row[7];
+        $output[$row[7]][$column]['fragenAntworte'][$fragen] = $row[2];
         $CSVData[$column] = $output;
 
     }
