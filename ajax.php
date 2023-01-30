147d15c00138b740b402d38a5177825362ebccc0
mamadidiarrisso <diarrisso49@gmail.com>
2023-01-30 23:33:07 +0100

    update tkschat ignore sum array, regrouper les chats by Datum et par date_format,array splite

--- ajax.php
+++ ajax.php
@@ -23,190 +23,106 @@ if( $_FILES['upload']['error'] == 0 ) {
     $tmpSize = $_FILES['upload']['size'];
 
     $csv_data = array_map('str_getcsv', file($tmpName));
-
-    /**
-     *
-     * array_shift = remove first value of array
-     * in csv file header was the first value
-     *
-     */
-
-
     $result = null;
-    $teste =  array( array( '12345566666',
-                     'diarrisso',
-                     'saran',
-                     4,
-                     4,
-                     4,
-                     4
-                 ),
-             );
+    $teste =  [
+         array('mamadi',1,2,3,0.00,5.67,6.56,7.56),
+        array('mamadi',1,2,3,4,5,6,7.600),
+        array('mamadi',1,2,3,4,5,6,7),
+        array('mamadi',1,2,3,4,5,6,7),
+        array('mamadi',1,2,3,4,5,6,7),
+        array('eva',150,25,35,45,55,65,75),
+        array('eva',15,25,35,45,56,66,79),
+        array('eva',14,25,36,47,57,67,77),
+        array('eva',14,25,36,47,57,67,77),
+        array('eva',14,25,36,47,57,67,77),
+    ];
     $newArray = array();
-    $data = array();
+    $sum1 = 0;
+    $sum2 = 0;
+    $sum3 = 0;
+    $sum4 = 0;
+    $sum5 = 0;
+    $sum6 = 0;
+    $sum7 = 0;
+    function sumValues($item) {
+        $carry = 0;
+        $carry += $item;
+        return $carry;
+    }
+
+
+    $sum = array_fill_keys(array_keys(reset($teste)), 0);
+
+    foreach($sum as $key => &$value) {
+        $value = array_sum(array_column($teste, $key));
+    }
+
+
+
+    $sum = [];
+
+    foreach($teste as $item) {
+        foreach($item as $key => $values) {
+            if (!isset($sum[$key])) $sum[$key] = 0;
+            $sum[$key] += $values;
+            $name = $values[0];
+            $sum[$key]['nom'] += $values[1];
+            $sum[$key]['prenom'] += $values[2];
+            $sum[$key]['int1'] += $values[3];
+            $sum[$key]['int2'] += $values[4];
+            $sum[$key]['int3'] += $values[5];
+            $sum[$key]['int4']+= $values[6];
+            $sum[$key]['int5'] += $values[7];
+            $newArray[] = $sum;
+        }
+    }
+
+
+
+
+    error_log('$message'.print_r($newArray,true));
+    print_r($sum);
+    die();
+
+
+
     foreach ( $teste as $key => $item ) {
 
-        error_log('items dans foreach  => ' . print_r($item, true));
-
-        $data['id'] = $item[0];
-        $data['nom'] = $item[1];
-        $data['prenom'] = $item[2];
-        $data['int1'] = $item[3];
-        $data['int2'] = $item[4];
-        $data['int3'] = $item[5];
-        $data['int4'] = $item[6];
-        $newArray = $data;
-        $output = array_slice( $newArray, 1);
-        $result = array_sum($output);
+        /*$data[$name]['nom'] = sumValues($item[1]);
+        $data[$name]['prenom'] = sumValues($item[2]);
+        $data[$name]['int1'] =sumValues($item[3]);
+        $data[$name]['int2'] = sumValues($item[4]);
+        $data[$name]['int3'] = sumValues($item[5]);
+        $data[$name]['int4']=  sumValues($item[6]);
+        $data[$name]['int5'] = sumValues($item[7]);
+        $newArray = $data;*/
 }
-        $newArray['sum'] = $result;
-    error_log('$rsulte de =>'.print_r( $result, true ) );
-    error_log('$data =>'.print_r( $newArray, true ) );
-    error_log('$output =>'.print_r( $output, true ) );
 
+
+
+
+
+    error_log('$data first => '.print_r( $newArray, true ) );
     die();
-    //array_shift($csv_data);
 
     $CSVData = array();
     $output = array();
     foreach ( $csv_data as $key => $row ) {
-        $separator = ':';
-        $fragen = explode( $separator, $row[1]) ;
-        /*$nom = explode(" ", $row[4]);
-        $prenom = $nom[0];
-        $nachnme = $nom[1];*/
-        $fragen = trim( $fragen[0]);
-        $column = $row[0];
-        unset($row[0]);
-        $csvDatum = "2023-01-11";
 
-        /*if ($row[7] != $csvDatum ) {
-            continue;
-        }*/
+        $column = $row[3];
 
-        if (!in_array($csvDatum, $row) ) {
-            continue;
-            //error_log('oui il \'est pas dedant ');
-
-        }
-
-        if ($key === 1) {
-
-            continue;
-
-        }
-
-        $result = array_sum($row);
-
-        error_log('datum =>'.print_r( $row[7] ) );
-        error_log('resulte =>'.$result);
-        $date = date_create($row[7]);
-        $newDatas = date_format($date, 'Y-m-d');
-        $output[$row[7]]['benitzer'] = $row[6];
-        $output[$row[7]]['nom'] = $row[5];
-        $output[$row[7]]['studie'] = $row[8];
-        $output[$row[7]]['email'] = $row[3];
-        $output[$row[7]]['email'] = $row[4];
-        $output[$row[7]]['queue'] = $row[9];
-        $output[$row[7]]['date'] = $row[7];
-        $output[$row[7]][$column]['fragenAntworte'][$fragen] = $row[2];
+        $output[$row[3]]['benitzer'] = $row[6];
+        $output[$row[3]]['nom'] = $row[5];
+        $output[$row[3]]['studie'] = $row[8];
+        $output[$row[3]]['email'] = $row[3];
+        $output[$row[3]]['email'] = $row[4];
+        $output[$row[3]]['queue'] = $row[9];
+        $output[$row[3]]['date'] = $row[7];
         $CSVData[$column] = $output;
-
-    }
-    error_log(' array => '.print_r($CSVData, true));
-
-    /*if ( strpos($row[8], 'Wholebuy') ) {
-        error_log('business = > business WB');
-        echo 'business WB';
-    } else {
-        error_log('business ');
-        echo 'business ';
     }
 
-
-
-    if (! strpos($row[8], 'Wholebuy') ) {
-        error_log('business TKS');
-        echo 'business WB';
-    }
-
-
-    if ( strpos($row[1], "210", 1) ) {
-        error_log('offset');
-        echo 'offset';
-    }
-
-
-    $start = $row[1];
-
-    if ( $start[0] === "2" && $start[1] === "1" && $start[2] === "0" ){
-        error_log("coole");
-
-    }
-
-    if ($start[0] === '4' && $start[1] === '1' && $start[2] === '0') {
-        error_log('coole');
-        exit();
-    }
-
-
-if (strlen($row[1]) === "210") {
-error_log("salut ");
+    error_log(' array  csvdata => '.print_r($CSVData, true));
 }
-
-
-    $nummer  = explode(':', $row[1]);
-
-          $c = '5';
-
-    if ($row[0]) {
-        $interwiewIde = $row[0];
-    }
-
-    if ($row[5]) {
-        $vormane = $row[5];
-    }
-
-    if ($row[6]) {
-        $nachname = $row[6];
-    }*/
-
-
-
-
-    /*switch ($nummer) {
-
-        case "2100000002000400":
-
-
-            break;
-        case '2100000002001800':
-            echo 'ja';
-            break;
-        default :
-            'cool';
-    }*/
-
-
-    //error_log('le premier foreach => '  .print_r( $CSVData, true ) );
-
-
-
-
-
-    /*  $data = array();
-    foreach ( $CSVData  as  $interviewId => $value ) {
-
-        $data['interviewId'] = $interviewId ;
-        $data = $value;
-
-
-    }*/
-
-
-
-}
-
 
 function array_group( array $data, $by_column )
 {
